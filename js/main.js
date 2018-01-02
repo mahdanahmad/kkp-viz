e66966545476050487e2697e788e592549f73bc1
Mahdan Ahmad Fauzi Al-Hasan <mahdan.hasan@un.or.id>
2018-01-02 15:07:05 +0700

    sync with new file

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ const colors	= ['#94ac6c', '#d4b44c', '#ccc4bc', '#6c1414', '#64bcdc', '#a46c54'
 
 $.get('public/data.csv', ( read ) => {
 	const raw		= d3.dsvFormat(';').parse(read);
-	const unit		= _.chain(raw).map('unit').uniq().value();
+	const unit		= _.chain(raw).map('kedeputian').uniq().value();
 	const palette	= _.chain(unit).map((o, i) => ([o, colors[i]])).fromPairs().value();
 	const belanja	= _.chain(raw).map('belanja').uniq().value();
-	const formatted	= _.chain(raw).groupBy('unit').mapValues((o) => (_.chain(o).keyBy('belanja').mapValues('anggaran').value())).value();
+	const formatted	= _.chain(raw).groupBy('kedeputian').mapValues((o) => (_.chain(o).groupBy('direktorat').mapValues((d) => (_.chain(d).keyBy('belanja').mapValues((m) => (parseInt(m.anggaran))).value())).value())).value();
 
 	$( document ).ready(() => {
-		createSide(unit, palette, _.mapValues(formatted, (o) => (_.chain(o).map((d) => (parseInt(d))).sum().value())));
+		createSide(unit, palette, _.mapValues(formatted, (o) => (_.chain(o).get('Total').values().sum().value())));
 	});
 });
