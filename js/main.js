52ea4f46e22360f8f59ec4f5814ec1d004d4d91d
Mahdan Ahmad Fauzi Al-Hasan <mahdan.hasan@un.or.id>
2018-04-26 14:05:14 +0700

    no total

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 const colors	= ['#94ac6c', '#d4b44c', '#ccc4bc', '#98DBC6', '#64bcdc', '#a46c54', '#c44434', '#fcb47c', '#bc6494'];
 
-$.get('public/data.csv', ( read ) => {
+$.get('public/nototal.csv', ( read ) => {
 	const raw		= d3.dsvFormat(';').parse(read).map((o) => _.assign(o, { anggaran: parseInt(o.anggaran) }));
 	const unit		= _.chain(raw).map('kedeputian').uniq().value();
 	// const formatted	= _.chain(raw).groupBy('kedeputian').mapValues((o) => (_.chain(o).groupBy('direktorat').mapValues((d) => (_.chain(d).keyBy('belanja').mapValues((m) => (parseInt(m.anggaran))).value())).value())).value();
 
-	const sideData		= _.chain(raw).filter(['direktorat', 'Total']).groupBy('kedeputian').mapValues((o) => _.sumBy(o, 'anggaran')).value();
-	const scatterData	= _.chain(raw).filter(['direktorat', 'Total']).map((o) => _.pick(o, ['kedeputian', 'belanja', 'anggaran'])).value();
+	const sideData		= _.chain(raw).groupBy('kedeputian').mapValues((o) => _.sumBy(o, 'anggaran')).value();
+	const scatterData	= _.chain(raw).groupBy((o) => (o.kedeputian + '|' + o.belanja)).mapValues((o) => (_.sumBy(o, 'anggaran'))).map((o, key) => ({ kedeputian: key.split('|')[0], belanja: key.split('|')[1], anggaran: o })).value();
 
 	belanja			= _.chain(raw).map('belanja').uniq().value();
-	barData			= _.chain(raw).filter(['direktorat', 'Total']).map((o) => _.pick(o, ['kedeputian', 'belanja', 'anggaran'])).groupBy('kedeputian').value();
-	kedepData		= _.chain(raw).reject(['direktorat', 'Total']).groupBy('kedeputian').value();
+	barData			= _.groupBy(scatterData, 'kedeputian');
+	kedepData		= _.groupBy(raw, 'kedeputian');
 	palette			= _.chain(unit).map((o, i) => ([o, colors[i]])).fromPairs().value();
 
 	$( 'span#total-anggaran' ).text(nFormatter(_.chain(sideData).values().sum().value()));
